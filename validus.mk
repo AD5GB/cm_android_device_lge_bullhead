170c08c7e9dae3acd3b2a0a6bfaf4292824cc420
Randy DuCharme <radio.ad5gb@gmail.com>
2017-02-04 03:11:42 -0600

    Add CAF and switch setup for Validus
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

--- validus.mk
+++ validus.mk
@@ -4,6 +4,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Tesla stuff.
 $(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
